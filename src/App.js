d7f3b978e3d1bbd435a7ac41b8591c983f78beca
jrafaelcenit <rafael.cenit@elca.ch>
2019-09-09 15:49:03 +0200

    Unit tests added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Hello world!</h1>
-        </header>
+      <div data-test="component-app">
+       <h1 data-test="counter-display">The counter is: {this.state.counter}</h1>
+       <button onClick={() => this.setState({ counter: this.state.counter + 1 })} 
+       data-test="increment-button">Increment counter</button>
       </div>
     );
   }
